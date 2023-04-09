1ef26e0bb68c3e1a9583dfbc946f8a2a8d697c17
Binu Abraham <binuabraham990@gmail.com>
2023-04-09 20:43:26 +0530

    Multiple changes done

--- app.js
+++ app.js
@@ -12,7 +12,6 @@ app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 app.use(express.json());
 app.use(express.urlencoded({extended: false}));
-app.use(express.static(path.join(__dirname, 'public')));
 app.use(
     session({ cookie: { maxAge: 60000 }, 
     secret: 'woot',
--- routes/credentials.js
+++ routes/credentials.js
@@ -65,4 +65,65 @@ router.post('/add', function(req, res, next) {
     }
 
 });
+
+router.get('/show/(:id)', function(req, res, next) {
+
+    let id = req.params.id;
+    dbCon.query('Select * from credentials where id = ? limit 1', parseInt(id), function(err, rows) {
+        if(!err) {
+            res.render('credentials/show', {data: rows[0]});
+        }
+
+    });
+});
+
+router.get('/update/(:id)', function(req, res, next) {
+
+    let id = req.params.id;
+    dbCon.query('Select * from credentials where id = ? limit 1', parseInt(id), function(err, rows) {
+        if(!err) {
+            let data = {
+                id: req.params.id,
+                website: rows[0].website,
+                service: rows[0].service,
+                username: rows[0].username,
+                password: rows[0].password,
+                description: rows[0].description
+            }
+
+            res.render('credentials/update',data);
+        }
+
+    });
+
+});
+
+router.post('/update/(:id)', function(req, res, next) {
+    let id = req.params.id;
+    let {website, service, username, password, description} = req.body;
+    let data = {
+        id: req.params.id,
+        website: website,
+        service: service,
+        username: username,
+        password: password,
+        description: description
+    }
+
+    let errors = false;
+    if(website == '' || service == '' || username == '')    {
+        let errors = true;
+        req.flash('error', 'Please insert all required columns');
+
+        res.render('credentials/update',data);
+    }
+
+    if(!errors) {
+        dbCon.query('update credentials SET ? WHERE id = ?' , [data, id], function(err, result)   {
+            if(!err)    {
+                res.redirect('/credentials/');
+            }
+        });
+    }
+});
 module.exports = router;
